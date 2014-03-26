c6a806616e732a65bf7ab7c4bb970470c8dfa10d
tabalt <tabalt@actphp.com>
2014-03-27 00:07:39 +0800

    add upload plugin source

---
+++ src/upload.js
@@ -0,0 +1,105 @@
+/**
+ * @file upload.js
+ * @desc Jquery File Upload Plugin
+ * @licensed http://www.apache.org/licenses/LICENSE-2.0
+ * @auth Tabalt <tabalt@actphp.com>
+ * @version 1.1 
+ * @update 2014.03.26
+ * @import utils.js
+ */
+
+!function($){
+
+	var defaults = {
+        width : 100, 
+        height : 30, 
+        name : 'upfile',
+        uploadUrl : '',//上传地址
+        type : 'image/gif,image/jpeg,image/png,image/jpg,image/bmp',//选择的类型
+        btnBackgroundImage : 'upload_btn_img.png', //上传按钮背景图片
+        btnStyleClass : '', //上传按钮样式
+        target: 'grape-upload-target-' + Utils.Random.GetRandomId(),
+        startCallback : null,
+        successCallback : null,
+        errorCallback : null
+    };
+
+    $.fn.upload = function(options){
+
+    	var settings = $.extend({}, defaults, options);
+    	var uploader = $(this);
+    	var uploadHtml = '<div class="grape-upload-box" style="width:' + settings.width + 'px;height:' + settings.height 
+			+ 'px;" ><form class="grape-upload-form" method="post" enctype="multipart/form-data" '
+			+ ' target="' + settings.target + '" action="' + settings.uploadUrl +'">' 
+			+ '<input contentEditable="false" style="display:none;" id="grape-upload-file" class="grape-upload-file" type="file" ' 
+			+ ' hidefocus="" title="" name="' + settings.name +'" accept="' + settings.type + '" />'
+			+ '<input class="grape-upload-button ' + settings.btnStyleClass +'" style="background:url('+ settings.btnBackgroundImage +') no-repeat; width:' + settings.width + 'px;height:' + settings.height 
+			+ 'px; /* filter: alpha(opacity=0);opacity:0;*/ border:none; cursor:pointer;" type="button" value="" />'
+			+ '</form></div>';
+
+		var uploadHtmlForIe = '<div class="' + settings.btnStyleClass +'" style="background:url('+ settings.btnBackgroundImage +') no-repeat; width:' + settings.width 
+			+ 'px;height:' + settings.height  + 'px;" >'
+			+ '<form class="grape-upload-form" method="post" enctype="multipart/form-data" '
+			+ ' target="' + settings.target + '" action="' + settings.uploadUrl +'">' 
+			+ '<input contentEditable="false" style="filter: alpha(opacity=0);opacity:0; border:none;width:' + settings.width 
+			+ 'px;height:' + settings.height + 'px;" class="grape-upload-file" type="file" ' 
+			+ ' hidefocus="" title="" name="' + settings.name +'" accept="' + settings.type + '" /></form></div>';
+		
+		if( Utils.Broswer.IsFirefox || Utils.Broswer.IsWebkit || Utils.Broswer.IsOpera ) {
+			$(uploadHtml).appendTo(uploader);
+			uploader.find(".grape-upload-button").click(function(){
+				uploader.find(".grape-upload-file").click();
+			});
+		} else {
+			$(uploadHtmlForIe).appendTo(uploader);
+		}
+
+		uploader.find(".grape-upload-file").change(function(){
+			if($(this).val()) {
+
+				//上传开始 回调
+				settings.startCallback && settings.startCallback();
+			
+				//生成一个隐藏iframe，并设置form的target为该iframe，模拟ajax效果
+				var iframe = $('<iframe id="' + settings.target + '" name="' + settings.target + '" style="display: none"></iframe>');
+				var form = $(this).parent()[0];
+				iframe.appendTo($('body'));
+
+				//设置 上传完毕iframe onload事件
+				iframe.load(function(){
+					//获取上传结果
+					var response = this.contentWindow.document.body.innerHTML;
+					response = Utils.String.ReplaceAll(response, '&amp;', '&');
+					form.reset();
+					iframe.remove();
+					//清除引用
+					iframe = null; 
+
+					//解析上传结果
+					var responseData = {
+						status : 0,
+						info : '',
+						data : null
+					};
+					try{
+						responseData = $.parseJSON(response);
+					} catch (e){
+
+					}
+
+					//处理上传结果
+					if(responseData && responseData.status == 1) {
+						//上传成功  回调
+						settings.successCallback && settings.successCallback(responseData);
+					} else {
+						//上传失败  回调
+						settings.errorCallback && settings.errorCallback(responseData);
+					}
+				});
+
+				//提交表单上传文件
+				form.submit();
+			}
+		});
+    };
+}(window.jQuery);
---
+++ src/utils.js
@@ -0,0 +1,75 @@
+/**
+ * @file utils.js
+ * @desc 静态工具函数
+ */
+var Utils = {
+	/**
+	 * 	类型判断
+	 */
+	Type : {
+		CheckType : function (element, typeName) {
+			if (typeof (element) == typeName ) {
+				return true;
+			} else {
+				return false;
+			}
+		},
+		IsObject : function (element) {
+			return this.CheckType(element, 'object');
+		},
+		IsString : function (element) {
+			return this.CheckType(element, 'string');
+		},
+		IsBoolean : function (element) {
+			return this.CheckType(element, 'boolean');
+		},
+		IsNumber : function (element) {
+			return this.CheckType(element, 'number');
+		},
+	},
+	String : {
+		LTrim : function (str) {
+			var i;
+			for (i = 0; i < str.length; i++) {
+				if (str.charAt(i) != " " && str.charAt(i) != " "){
+					break;
+				}
+			}
+			str = str.substring(i, str.length);
+			return str;
+		},
+		RTrim : function (str) {
+			var i;
+			for (i = str.length - 1; i >= 0; i--) {
+				if (str.charAt(i) != " " && str.charAt(i) != " "){
+					break;
+				}
+			}
+			str = str.substring(0, i + 1);
+			return str;
+		},
+		Trim : function (str) {
+			return this.RTrim(this.LTrim(str));
+		},
+		ReplaceAll : function(str, search, replace) {
+		    return str.replace(new RegExp(search, "gm"), replace); 
+		}
+	},
+	Random : {
+		GetRandomNum : function (Min, Max) {
+			var Range = Max - Min;
+			var Rand = Math.random();
+			return (Min + Math.round(Rand * Range));
+		},
+		GetRandomId : function () {
+			return this.GetRandomNum(1, 99999) + this.GetRandomNum(1, 99999) + this.GetRandomNum(1, 99999);
+		}
+	},
+	Broswer : {
+		IsFirefox : /firefox/.test(navigator.userAgent.toLowerCase()),
+		IsWebkit :  /webkit/.test(navigator.userAgent.toLowerCase()),
+		IsOpera : /opera/.test(navigator.userAgent.toLowerCase()),
+		IsIe : /msie/.test(navigator.userAgent.toLowerCase()),
+	},
+	
+};
